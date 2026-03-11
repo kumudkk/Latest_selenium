23ab88d38ecf9af7f3d70c9d405e06115302b8c1
kumudkk <kunjilwark@gmail.com>
2026-03-11 17:46:21 -0500

    Second commit

---
+++ src/test/java/UI/Test2.java
@@ -0,0 +1,19 @@
+package UI;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class Test2 {
+
+	WebDriver driver;
+	
+	@Test(priority=2)
+	public void test2() {
+		System.out.println("This is my second test");
+	}
+	
+	@Test(priority=1)
+	public void test3() {
+		System.out.println("This is my third test");
+	}
+}
