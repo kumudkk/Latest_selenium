0e1e6f39dc476ac27f95b0c43746708811f47298
kumudkk <kunjilwark@gmail.com>
2026-03-11 17:55:26 -0500

    More code added to Test2

--- src/test/java/UI/Test2.java
+++ src/test/java/UI/Test2.java
@@ -1,6 +1,8 @@
 package UI;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
 public class Test2 {
@@ -9,11 +11,19 @@ public class Test2 {
 	
 	@Test(priority=2)
 	public void test2() {
-		System.out.println("This is my second test");
+		driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		System.out.println("Launched the browser and navigated to google");
 	}
 	
 	@Test(priority=1)
 	public void test3() {
 		System.out.println("This is my third test");
+	}
+	
+	@AfterTest
+	public void tearDown() {
+		driver.quit();
+		System.out.println("Closed the browser");
 	}
 }
