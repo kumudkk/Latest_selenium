b051656368c5e55e266a1f7d3cec403fa3c438dd
kumudkk <kunjilwark@gmail.com>
2026-03-11 16:35:34 -0500

    Initial commit

---
+++ src/test/java/UI/First_Test.java
@@ -0,0 +1,14 @@
+package UI;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class First_Test {
+	WebDriver driver;
+	
+	@Test
+	public void test1() {
+		System.out.println("This is my first test");
+	}
+
+}
